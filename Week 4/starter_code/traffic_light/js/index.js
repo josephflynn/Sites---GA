e8a4a4990165e76faf23b19adbb511fb8d0395f2
josephflynn <joseph.f.flynn@gmail.com>
2017-03-29 13:26:06 +1100

    Assessment - jQuery

---
+++ Week 4/starter_code/traffic_light/js/index.js
@@ -0,0 +1,27 @@
+//Implement the red light using jQuery. Don't forget to add the script tags.
+
+console.log("hello world");
+
+$(document).ready(function(){
+
+	function clear(){
+
+		$(".bulb").attr("class", "bulb");
+	}
+
+	$("#stopButton").click(function(){
+		clear();
+		$("#stopLight").addClass("red");
+	});
+
+	$("#slowButton").click(function(){
+		clear();
+		$("#slowLight").addClass("orange");
+	});
+
+	$("#goButton").click(function(){
+		clear();
+		$("#goLight").addClass("green");
+	});
+
+});
